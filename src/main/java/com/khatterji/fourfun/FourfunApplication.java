1a957644fcb6928a045fd45437f55cff0e8e6277
Jamal Khattab <jamalkhattab1@gmail.com>
2019-05-01 15:59:20 +0300

    Adding new files
    adds the right files

---
+++ src/main/java/com/khatterji/fourfun/FourfunApplication.java
@@ -0,0 +1,13 @@
+package com.khatterji.fourfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourfunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourfunApplication.class, args);
+	}
+
+}
